065db556ee9540809c5ebd2d58c15ab649decce6
Leandro Ezequiel Pedercini <leandroezequielpedercini@gmail.com>
2025-04-04 00:47:05 -0300

    -

--- script.Cabanias.js
+++ script.Cabanias.js
@@ -42,29 +42,31 @@ parrafoLascabanias.innerHTML = `<p class="parra-uno-de-lascabanias" >El complejo
 
   /* imagen 1 */
 const img1 = document.getElementById("img-Cabania-Paras-Seis")
-img1.innerHTML = `<img src="img/cabañas/1/Cabaña de 6 personas.jpg" alt="Cabañas para 6 personas" class="cabania-para-seis-personas"onclick="btncaba6()">`
+img1.innerHTML = ` <img src="img/cabañas/1/Cabaña de 6 personas.jpg" alt="Cabañas para 6 personas" class="cabania-para-seis-personas"onclick="btncaba6()">`
 
-
-
-
-
-{/* <img src="img/cabañas/3/cruz.png" alt="Cerrar ventana"  class="ventanaCruz"  onclick="ventanacierre()""> */}
-
-
+/* Imagen de la cabaña 6 */
 const carro1 = document.getElementById("carritoId6")
-carro1.innerHTML = `
+carro1.innerHTML = `<img src="img/cabañas/1/cruz6.png" alt="Cerrar Ventana" id="btn-Cerrar6" class="btnCerrar6" onclick="btnCerrar6()">
 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="img/cabañas/1/Cabaña de 6 personas.jpg" class="d-block w-100" alt="...">
+  <div class="carousel-inne6">
+    
+  <div class="carousel-item active">
+      <img src="img/cabañas/1/1.jpg" class="d-block w-100" alt="foto 1">
     </div>
+  
     <div class="carousel-item">
-      <img src="img/cabañas/1/Cabaña de 6 personas.jpg" class="d-block w-100" alt="...">
+      <img src="img/cabañas/1/2.jpeg" class="d-block w-100" alt="...">
+      </div>
+  
+      <div class="carousel-item">
+      <img src="img/cabañas/1/3.jpg" class="d-block w-100" alt="...">
+     </div>
+
+  <div class="carousel-item">
+      <img src="img/cabañas/1/3.jpg" class="d-block w-100" alt="mklgdj">
     </div>
-    <div class="carousel-item">
-      <img src="img/cabañas/1/Cabaña de 6 personas.jpg" class="d-block w-100" alt="...">
+
     </div>
-  </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
@@ -75,20 +77,110 @@ carro1.innerHTML = `
   </button>
 </div>`
 
+/* Boton de la imagen de la canaña 6 */
 
-
- function btncaba6(){
-   const carrusel6 = document.querySelector(".classCarrito6-1-");
-  carrusel6.classList.replace("classCarrito6-1-", "classCarrito6-2")
-  console.log(body) 
-  
+function btncaba6(){
+const carrusel6 = document.querySelector(".classCarrito6-1-");
+carrusel6.classList.replace("classCarrito6-1-", "classCarrito6-2") 
 } 
 
+/* Boton para cerrar la ventana */
 
-function ventanacierre(){
-  const cerrar6 = document.querySelector(".classCarrito6-2-")
-  cerrar6.classList.replace ("ventanaCruz", "ventanaCruzCierre")
+function btnCerrar6(){
+const cerrarVentana6 = document.querySelector(".classCarrito6-2")
+cerrarVentana6.classList.replace("classCarrito6-2","classCarrito6-3")
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* imagen 2 */
+const img2 = document.getElementById("img-Cabania-Paras-Cuatro")
+img2.innerHTML = `<img src="img/cabañas/2/Cabaña de 4 personas.jpg" alt="Cabañas para 4 personas" class="cabania-para-seis-personas"onclick="btncaba6() > `
+                    
+/* Imagen de la cabaña 4 */
+const carro2 = document.getElementById("carritoId4")
+carro2.innerHTML = `<img src="img/cabañas/4/cruz6.png" alt="Cerrar Ventana" id="btn-Cerrar4" class="btnCerrar4" onclick="btnCerrar4()">
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    
+  <div class="carousel-item active">
+      <img src="img/cabañas/4/4.jpg" class="d-block w-100" alt="foto 1">
+    </div>
+  
+    <div class="carousel-item">
+      <img src="img/cabañas/4/4.jpeg" class="d-block w-100" alt="...">
+      </div>
+  
+      <div class="carousel-item">
+      <img src="img/cabañas/4/4.jpg" class="d-block w-100" alt="...">
+     </div>
+
+  <div class="carousel-item">
+      <img src="img/cabañas/4/4.jpg" class="d-block w-100" alt="mklgdj">
+    </div>
+
+    </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>`
+
+/* Boton de la imagen de la canaña 4 */
+
+function btncaba4(){
+const carrusel4 = document.querySelector(".classCarrito4-1-");
+carrusel4.classList.replace("classCarrito4-1-", "classCarrito4-2") 
+} 
+
+/* Boton para cerrar la ventana */
+
+function btnCerrar4(){
+const cerrarVentana4 = document.querySelector(".classCarrito4-2")
+cerrarVentana4.classList.replace("classCarrito4-2","classCarrito4-3")
+}
+
+
+
+
+
+
+
+
 
 
 
